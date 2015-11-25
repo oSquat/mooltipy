a5af01f8e620590c4d3c3d41792edb1a2544cbf3
Victor Simeone <github@osquat.com>
2015-11-24 22:36:10 -0500

    Fix mpfavorites set using old node property

--- mooltipy/utilities/mpfavorites.py
+++ mooltipy/utilities/mpfavorites.py
@@ -77,13 +77,16 @@ def set_favorite(mooltipass, args):
     while favorite_slot >= 14:
         favorite_slot = int(input("Please select a favorite slot(0-13):"))
 
-    print('Putting {}:{} in favorite slot {}'.format(ctx_favorite_list[selected_favorite][0].service_name, ctx_favorite_list[selected_favorite][1].login, favorite_slot))
-    print(ctx_favorite_list[selected_favorite][0].node_addr, ctx_favorite_list[selected_favorite][1].node_addr)
-    logging.debug('Parent Addr:0x{:x} Child Addr:0x{:x}'.format(ctx_favorite_list[selected_favorite][0].node_addr, ctx_favorite_list[selected_favorite][1].node_addr))
+    print('Putting {}:{} in favorite slot {}'.format(
+        ctx_favorite_list[selected_favorite][0].service_name,
+        ctx_favorite_list[selected_favorite][1].login, favorite_slot))
+    logging.debug('Parent Addr:0x{:x} Child Addr:0x{:x}'.format(
+        ctx_favorite_list[selected_favorite][0].addr,
+        ctx_favorite_list[selected_favorite][1].addr))
 
     mooltipass.set_favorite(favorite_slot,
-                            (ctx_favorite_list[selected_favorite][0].node_addr,
-                             ctx_favorite_list[selected_favorite][1].node_addr))
+                            (ctx_favorite_list[selected_favorite][0].addr,
+                             ctx_favorite_list[selected_favorite][1].addr))
 
 def del_favorite(mooltipass, args):
     """Removes a favorite from the specified slot"""
