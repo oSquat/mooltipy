b518917422143417f1dfca55c7abdfe6192c870c
dicebox <gnudicebox@gmail.com>
2020-08-25 19:35:58 +0200

    python3 conversion: Convert strings to bytes before concatenation as Python3 rejects concatenating bytes to a string. Enforce ASCII encoding.

--- mooltipy/mooltipass.py
+++ mooltipy/mooltipass.py
@@ -291,7 +291,7 @@ class _Mooltipass(object):
             3 -- No card inserted into mooltipass
         """
 
-        self.send_packet(CMD_CONTEXT, array('B', context + b'\x00'))
+        self.send_packet(CMD_CONTEXT, array('B', bytes(context, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet(10000)
         return recv[0]
 
@@ -324,7 +324,7 @@ class _Mooltipass(object):
 
         Return 1 or 0 indicating success or failure.
         """
-        self.send_packet(CMD_SET_LOGIN, array('B', login + b'\x00'))
+        self.send_packet(CMD_SET_LOGIN, array('B', bytes(login, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet()
         return recv[0]
 
@@ -333,7 +333,7 @@ class _Mooltipass(object):
 
         Return 1 or 0 indicating success or failure.
         """
-        self.send_packet(CMD_SET_PASSWORD, array('B', password + b'\x00'))
+        self.send_packet(CMD_SET_PASSWORD, array('B', bytes(password, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet()
         return recv[0]
 
@@ -349,7 +349,7 @@ class _Mooltipass(object):
         # A timer blocks repeated checking of passwords.
         # A return of 0x02 means the timer is still counting down.
         while recv is None or recv == 0x02:
-            self.send_packet(CMD_CHECK_PASSWORD, array('B', password + b'\x00'))
+            self.send_packet(CMD_CHECK_PASSWORD, array('B', bytes(password, 'ascii') + b'\x00'))
             recv, _ = self.recv_packet()
             recv = recv[0]
             time.sleep(.2)
@@ -361,7 +361,7 @@ class _Mooltipass(object):
 
         Return 1 or 0 indicating success or failure.
         """
-        self.send_packet(CMD_ADD_CONTEXT, array('B', context + b'\x00'))
+        self.send_packet(CMD_ADD_CONTEXT, array('B', bytes(context, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet()
         return recv[0]
 
@@ -548,7 +548,7 @@ class _Mooltipass(object):
 
         Return 1 or 0 indicating success or failure.
         """
-        self.send_packet(CMD_SET_DATA_SERVICE, array('B', context + b'\x00'))
+        self.send_packet(CMD_SET_DATA_SERVICE, array('B', bytes(context, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet()
         return recv[0]
 
@@ -561,7 +561,7 @@ class _Mooltipass(object):
         Return 1 or 0 indicating success or failure.
         """
         print('sending ' + context)
-        self.send_packet(CMD_ADD_DATA_SERVICE, array('B', context + b'\x00'))
+        self.send_packet(CMD_ADD_DATA_SERVICE, array('B', bytes(context, 'ascii') + b'\x00'))
         recv, _ = self.recv_packet()
         return recv[0]
 
